65b1ac9bf92b0d69ec9465f695843e1593ee9668
Timmy Huy Xuan Ngo <tim.huyxuan.ngo@gmail.com>
2024-11-25 17:55:17 -0500

    Update driver.c

--- driver.c
+++ driver.c
@@ -24,92 +24,90 @@
 
 extern void Delay(uint32_t);
 
-char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-int choice, player;
-int checkForWin();
-void displayBoard();
-void mrkBoard(char mark);
+char cells[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int selection, player;
+int winCondition();
+void drawGameboard();
+void updateGameboard(char playerInput);
 
-int funcTicTacToe()
-{
+int funcTicTacToe() {
+ 
   int i;
-  char mark;
+  char playerInput;
   player = 1;
 
   GLCD_DisplayString(0, 4, __FI_LARGE, "Tic Tac Toe"); // Title
   Delay(3000000);
   
   do {
-    displayBoard();
+    drawGameboard();
     Delay(10000000);
     player = (player % 2) ? 1 : 2;
-    mark = (player == 1) ? 'X' : 'O';
-    mrkBoard(mark);
-    i = checkForWin();
+    playerInput = (player == 1) ? 'X' : 'O';
+    updateGameboard(playerInput);
+    i = winCondition();
     player++;
   } while (i == -1);
   
   return 0;
 }
 
-int checkForWin()
-{
-    int returnValue = 0;
-    if (square[1] == square[2] && square[2] == square[3])
-        returnValue = 1;
-    else if (square[4] == square[5] && square[5] == square[6])
-        returnValue = 1;
-    else if (square[7] == square[8] && square[8] == square[9])
-        returnValue = 1;
-    else if (square[1] == square[5] && square[5] == square[9])
-        returnValue = 1;
-    else if (square[3] == square[5] && square[5] == square[7])
-        returnValue = 1;
-    else if (square[1] == square[4] && square[4] == square[7])
-        returnValue = 1;
-    else if (square[2] == square[5] && square[5] == square[8])
-        returnValue = 1;
-    else if (square[3] == square[6] && square[6] == square[9])
-        returnValue = 1;
-    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-             square[4] != '4' && square[5] != '5' && square[6] != '6' &&
-             square[7] != '7' && square[8] != '8' && square[9] != '9')
-        returnValue = 0;
-    else
-        returnValue = -1;
-    return returnValue;
+int winCondition() {
+	 int returnValue = 0;
+	
+	 if (cells[1] == cells[2] && cells[2] == cells[3])
+   	returnValue = 1;
+  else if (cells[4] == cells[5] && cells[5] == cells[6])
+   	returnValue = 1;
+  else if (cells[7] == cells[8] && cells[8] == cells[9])
+   	returnValue = 1;
+  else if (cells[1] == cells[5] && cells[5] == cells[9])
+    returnValue = 1;
+  else if (cells[3] == cells[5] && cells[5] == cells[7])
+  		returnValue = 1;
+  else if (cells[1] == cells[4] && cells[4] == cells[7])
+    returnValue = 1;
+  else if (cells[2] == cells[5] && cells[5] == cells[8])
+    returnValue = 1;
+  else if (cells[3] == cells[6] && cells[6] == cells[9])
+    returnValue = 1;
+  else if (cells[1] != '1' && cells[2] != '2' && cells[3] != '3' &&
+           cells[4] != '4' && cells[5] != '5' && cells[6] != '6' &&
+           cells[7] != '7' && cells[8] != '8' && cells[9] != '9')
+    returnValue = 0;
+  else
+    returnValue = -1;
+ 
+  return returnValue;
 }
 
 /* Draw the game board here */
-void displayBoard()
-{
-  // GLCD Display: 320x240 pixels
+void drawGameboard() {
+ // GLCD Display: 320x240 pixels
+ GLCD_DisplayString
 }
 
-void mrkBoard(char mark)
-{
-    if (choice == 1 && square[1] == '1')
-        square[1] = mark;
-    else if (choice == 2 && square[2] == '2')
-        square[2] = mark;
-    else if (choice == 3 && square[3] == '3')
-        square[3] = mark;
-    else if (choice == 4 && square[4] == '4')
-        square[4] = mark;
-    else if (choice == 5 && square[5] == '5')
-        square[5] = mark;
-    else if (choice == 6 && square[6] == '6')
-        square[6] = mark;
-    else if (choice == 7 && square[7] == '7')
-        square[7] = mark;
-    else if (choice == 8 && square[8] == '8')
-        square[8] = mark;
-    else if (choice == 9 && square[9] == '9')
-        square[9] = mark;
-    else
-    {
-        printf("Invalid ");
-        player--;
-        getch();
-    }
+void updateGameboard(char playerInput) {
+ 
+ if (selection == 1 && cells[1] == '1')
+  cells[1] = playerInput;
+ else if (selection == 2 && cells[2] == '2')
+  cells[2] = playerInput;
+ else if (selection == 3 && cells[3] == '3')
+  cells[3] = playerInput;
+ else if (selection == 4 && cells[4] == '4')
+  cells[4] = playerInput;
+ else if (selection == 5 && cells[5] == '5')
+  cells[5] = playerInput;
+ else if (selection == 6 && cells[6] == '6')
+  cells[6] = playerInput;
+ else if (selection == 7 && cells[7] == '7')
+  cells[7] = playerInput;
+ else if (selection == 8 && cells[8] == '8')
+   cells[8] = playerInput;
+ else if (selection == 9 && cells[9] == '9')
+   cells[9] = playerInput;
+ else
+   player--;
+ 
 }
